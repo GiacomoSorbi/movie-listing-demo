a9ab5090cb8ba0977512038f62222e37cedf7afb
Giacomo Sorbi <giacomosorbi@Ajnaputer-2.local>
2019-03-22 10:45:15 +0000

    Updates

---
+++ precache-manifest.36ee7edd7ced8d338024c41355ab1c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bf9b424c3b8cd27da21",
+    "url": "/movie-listing-demo/static/css/main.1879d943.chunk.css"
+  },
+  {
+    "revision": "1bf9b424c3b8cd27da21",
+    "url": "/movie-listing-demo/static/js/main.32b014e8.chunk.js"
+  },
+  {
+    "revision": "08831dad1f34f91ecacc",
+    "url": "/movie-listing-demo/static/js/runtime~main.374ec37c.js"
+  },
+  {
+    "revision": "e14e30ae91d15de21b91",
+    "url": "/movie-listing-demo/static/js/2.cb7268be.chunk.js"
+  },
+  {
+    "revision": "d291744e36f148e94b174f0e7db90e73",
+    "url": "/movie-listing-demo/static/media/film-roll.d291744e.svg"
+  },
+  {
+    "revision": "8245dbe0df7453ee1ab169f1c9e36e1a",
+    "url": "/movie-listing-demo/index.html"
+  }
+];
--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const API = {
-  API_KEY: {
-    V3: '25407f7c807d02fb57fef8ee0586086c',
-    V4:
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNTQwN2Y3YzgwN2QwMmZiNTdmZWY4ZWUwNTg2MDg2YyIsInN1YiI6IjVjOTI3NzgyOTI1MTQxMGI2ZjllZjFiMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kJRCj6S5AQi3AWCvL7ZFTh7SycVpTGamIcr88jMkDUM',
-  },
-  BASE_URI: window.location.protocol + '//api.themoviedb.org/3/',
-  IMAGES_URI: window.location.protocol + '//image.tmdb.org/t/p/',
-}
